d62e1e6c1ec7853b1c4531dd88628ddbc012947a
Pedro López <pedrolopez@andradas.es>
2025-01-08 10:47:46 +0100

    plugin scaffolding

---
+++ tender-a-library.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Plugin Biblioteca (A)
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Añade la funcionalidad de biblioteca al core de wordpress, incluyendo libros, lectores, préstamos, etc
+ * Author:          Local Anarquista Magdalena
+ * Author URI:      https://localanarquistamagdalena.org
+ * Text Domain:     tender-a-library
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Tender_A_Library
+ */
+
+// Your code starts here.
